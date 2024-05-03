cf0d6899e5a61ae0ab1b8be70c074a1c5d48ffca
Preet Jain <64613681+Qwerty999123@users.noreply.github.com>
2024-05-04 02:23:14 +0530

    Update admin_services.dart

--- lib/features/admin/services/admin_services.dart
+++ lib/features/admin/services/admin_services.dart
@@ -23,7 +23,7 @@ class AdminServices{
     List<String> imageUrl = [];
     final userProvider = Provider.of<UserProvider>(context, listen: false);
     try {
-      final cloudinary = CloudinaryPublic('drwyrkxzt', 'dkkwoiwt');
+      final cloudinary = CloudinaryPublic('', ''); // Enter your own cloudinary info
 
       for (int i = 0; i < images.length; i++) {
         CloudinaryResponse res = await cloudinary.uploadFile(
